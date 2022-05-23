ae768e5a60aba5f711ec24fab7d8e1ca57a7e454
labCap <75088990+labCap@users.noreply.github.com>
2022-05-23 11:52:24 +0300

    autoplay+

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(function () {
         slidesToShow: 3,
         slidesToScroll: 1,
         swipe: true,
-        // dots: true,
+        autoplay: true,
         prevArrow: '<button type="button"><img src="img/left.png"></button>',
         nextArrow: '<button type="button"><img src="img/right.png"></button>',
         responsive: [
