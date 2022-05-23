fb476b547a2dd06f0bc62e1970bb1d0842d1de99
labCap <75088990+labCap@users.noreply.github.com>
2022-05-23 11:28:20 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 $(function () {
 
-    $('.btn__menu').on('click', function () {
-        $('.menu__list').slideToggle();
+    $('.pets__items').slick({
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        swipe: true,
+        // dots: true,
+        prevArrow: '<button type="button"><img src="img/left.png"></button>',
+        nextArrow: '<button type="button"><img src="img/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 400,
+                settings: { slidesToShow: 1, slidesToScroll: 1, }
+            }
+        ]
     });
-
-
 });
+$('.btn__menu').on('click', function () {
+    $('.menu__list').slideToggle();
+});
+
